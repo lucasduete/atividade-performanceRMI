736cad594cdd83f9d834705db1b1505d9dc75a29
Lucas Duete <lm.duete@gmail.com>
2019-02-11 20:10:28 -0300

    Kill identityManager process on press Enter key

--- identityManager/src/main/java/io/github/lucasduete/sd/atividade/performanceRMI/identityManager/Loader.java
+++ identityManager/src/main/java/io/github/lucasduete/sd/atividade/performanceRMI/identityManager/Loader.java
@@ -3,23 +3,30 @@ package io.github.lucasduete.sd.atividade.performanceRMI.identityManager;
 import io.github.lucasduete.sd.atividade.performanceRMI.interfaces.IdentityInterface;
 
 import java.rmi.AlreadyBoundException;
+import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.Scanner;
 
 public class Loader {
 
-    public static void main(String[] args) throws RemoteException, AlreadyBoundException {
+    public static void main(String[] args) throws RemoteException, AlreadyBoundException, NotBoundException {
+
+        final String name = "IdentifyManager";
 
         IdentityInterface manager = new IdentifyManager();
 
-        LocateRegistry.createRegistry(10099).bind("IdentifyManager", manager);
+        Registry registry = LocateRegistry.createRegistry(10099);
+        registry.bind(name, manager);
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Pressione enter para parar e listar IDs usados");
 
         scanner.nextLine();
         manager.listAllUsedIDs();
+
+        registry.unbind(name);
     }
 
 }
